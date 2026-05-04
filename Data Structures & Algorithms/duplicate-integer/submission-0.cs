8684a648b72c8ceae1138eceba6b12e376eeb334
techyaya <thijygilbertorak@gmail.com>
2026-05-04 11:02:55 +0200

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cs
@@ -0,0 +1,18 @@
+//using System.Collections.Generic;
+
+public class Solution {
+    public bool hasDuplicate(int[] nums) 
+    {
+
+        HashSet<int> numsFilter = new HashSet<int>(nums);
+        if(nums.Length == numsFilter.Count)
+        {
+            return false;
+        }
+        else {
+            return true;
+        }
+
+        
+    }
+}
